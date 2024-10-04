2643381d047fa1ea7c9cf2ddd6cb1057f4d9037a
octomagician <anadeleonazp@gmail.com>
2024-10-03 19:30:28 -0600

    Initial commit

---
+++ app/src/main/java/com/example/intends/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.intends;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+// importando lo que se ocupa
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    //creando instancias de cada Intent
+    Intent activity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // ------------------------------------------------------------------------
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        // ------------------------------------------------------------------------
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("¡Botón presionado!");
+            }
+        });
+
+    }
+    }
